d8166adff54ba16940ed873b2c2756d62a6abea2
mdfaisalmahamud <mdfaisalmollik7@gmail.com>
2023-02-14 23:18:10 +0600

    Mad Libs Game

---
+++ Mad Libs Game.py
@@ -0,0 +1,12 @@
+# Building A Mad Libs Game
+
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are", color)
+print(pluralNoun + " are blue")
+print("I love", celebrity)
+
+
+
